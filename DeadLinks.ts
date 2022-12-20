4ccb09d8f6a5feabb6226c0ecaaa6918ea2dd019
nikhil2207 <nikhil.jain.2005@gmail.com>
2022-12-20 17:26:23 +0530

    Updated the deadlink utility

--- DeadLinks.ts
+++ DeadLinks.ts
@@ -1,8 +1,12 @@
-// const { Worker } = require('worker_threads');
+const { Worker } = require('worker_threads');
 const {webkit} = require('playwright');
+const urlToCheck = "https://www.google.com/";
 const baseUrlCheck = "google.com";
 const urlLevelCheck = 2;
 const fetch = require('node-fetch');
+const fs = require('fs');
+
+
 findDeadLinks();
 
 async function findDeadLinks() {
@@ -11,7 +15,8 @@ async function findDeadLinks() {
   const page = await context.newPage();
   console.log ("Starting the utility");
 
-  await page.goto("https://www.google.com/");
+  await page.goto(urlToCheck);
+  const pageTitle = await page.title();
 
   //Base URL Check
   let links = "";
@@ -23,24 +28,13 @@ async function findDeadLinks() {
   }
 
   // Get all the links on the page
-  let hashMap = await getURLs(links,"href");
+  let hashMap = await getURLs(links,"href", pageTitle);
 
   hashMap  = await filterUrlLevelCheck(hashMap,urlLevelCheck);
-  // let response = await page.goto("https://www.mercerabc.com");
-  // console.log("Response : " + response.status());
-  // await fetchResponse("");
-  // hashMap.forEach (function(value, key) {
-    // console.log(" Response : " + fetch("https://www.googlfffse.com").then(function(response) {
-    //   return response.status;
-    // }).then(function(data) {
-    //   console.log(data);
-    // }).catch(function(err) {
-    //   console.log('Fetch Error :-S', err);
-    // }));
-  //     console.log("Response :" + response);
-  //   }))
-     
-  // })
+  
+  hashMap = await fetchResponse(hashMap);
+  
+  await createCSVFile(hashMap);
   
 }
 
@@ -51,16 +45,18 @@ async function getAttribute(element,attribute) {
   return textValue
 }
 
-async function getURLs(elements,attribute) {
+async function getURLs(elements,attribute,pageTitle) {
+  console.log("Fetching the URLs and saving in hash map")
   let hashMap =  new Map();
   for (let element of elements){
     let link = await getAttribute(element,attribute);
-    hashMap.set(link,"Alive");
+    hashMap.set(link,pageTitle);
   }
   return hashMap
 }
 
 async function filterUrlLevelCheck(hashmap,urlLevelCheck){ 
+  console.log("URL Count Before URL Level Check Filter : " + hashmap.size)
   if (urlLevelCheck>0){
     let map = new Map();
     hashmap.forEach (function(value, key) {
@@ -68,12 +64,53 @@ async function filterUrlLevelCheck(hashmap,urlLevelCheck){
         map.set(key,value);
       }
     })
+    console.log("URL Count After URL Level Check Filter : " + map.size)
     return map;
     }else{
+      console.log("URL Count After URL Level Check Filter : " + hashmap.size)
       return hashmap;
     }
   }
 
-  // async function fetchResponse(url){
-  //   console.log(" Response : " + await fetch("http://www.google.com").then(reponse => console.log("Response : " + reponse)))
-  // }
+
+  async function fetchResponse(hashMap){
+    let map = new Map();
+    let keys= "";
+    hashMap.forEach (async function(value, key) {
+      keys = keys + key + ",";  
+    });
+    let k = keys.split(",");
+    for (let i = 0; i < k.length-1; i++) {
+      let startTime = new Date().getTime();
+      let key = k[i];
+      let responseCode = "";
+      await fetch(key)
+      .then(response => responseCode =  response.status.toString())
+      .then(data => {
+        // do something with the API response data
+      })
+      .catch(error => {
+        console.log("data : " + error)
+        // handle any errors that occurred during the request
+      });
+      let endTime = new Date().getTime();
+      map.set(key,hashMap.get(key) + "," + responseCode + "," + (endTime-startTime));
+    }
+  
+    return map;
+  }
+
+  async function createCSVFile(hashMap){
+    let data = "URL,PageTitle,ResponseCode,ResponseTime(ms)\n";
+    hashMap.forEach (function(value, key) {
+        data = data + key + "," + value + "\n";
+    });
+    
+    fs.writeFile('Output.csv', data, (err) => {          
+        // In case of a error throw err.
+        if (err) throw err;
+    })
+    console.log("File Created");
+  }
+
+  
