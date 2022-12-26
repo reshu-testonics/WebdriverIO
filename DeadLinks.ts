08d694a3fa18691e3244d84d7b518c5a5cd3d757
nikhil2207 <nikhil.jain.2005@gmail.com>
2022-12-26 13:34:00 +0530

    Updated the utility

--- DeadLinks.ts
+++ DeadLinks.ts
@@ -1,32 +1,90 @@
-const { Worker } = require('worker_threads');
 const {webkit} = require('playwright');
-let urlToCheck = "https://www.google.com/";
-const baseUrlCheck = "google.com";
-const urlLevelCheck = 2;
 const fetch = require('node-fetch');
 const fs = require('fs');
+const controller = require('node-abort-controller');
+
+let xpathWithBaseUrlCheck; //Fetch the xpath filtering the URLs in baseUrlCheck variable
+let existingUrlsFlag = false; //If true, utility will run on the existing URLs from inputFile variable
+let inputFile = "./Input.csv"; //Input file name if URL data already exists
+let outputFile = "Output.csv"; //Output file name
+let urlToCheck = "https://www.google.com/"; //URL for which utility needs to be executed
+const baseUrlCheck = "google.com"; //URL will be fetched only containing the comma separate URLs in baseURLCheck
+const urlsAttribute = "href"; //Attribute name in the applciation containing URL
+const urlLevelCheck = 2; //Number of levels (Count of front Slash) for a URL to check
+const numberOfThreads = 10; //Number of parallel threads
+const pageLoadTimeout = 15000; //Page load time out
+const fetchResponseTimeout = 5000; //API response timeout
+const maxURLCheck = 50; //Make this check as blank if no limit is needed
+
 let urlNotCheckedMap =  new Map();
 let urlCheckedMap = new Map();
+let responseCheckedMap  = new Map();
 
 findDeadLinks();
 
+/*
+Description : This method finds the dead links in the URL given in the the global variable "urlToCheck"
+*/
 async function findDeadLinks() {
-  const browser = await webkit.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  console.log ("Starting the utility");
+  console.log ("Starting Find Dead Links Utility");
+  let arrayOfPromises = new Array(numberOfThreads);
+  if (existingUrlsFlag == false){
 
-  // Get all the links on the page
-  let hashMap = await getURLs(page);
-  // hashMap  = await filterUrlLevelCheck(hashMap,urlLevelCheck);
+    //Get Playwright page object
+    const browser = await webkit.launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    //Get the xpath of the URLs to be checked based on base url check
+    xpathWithBaseUrlCheck = getURLCheckXpath();
+
+    //Get the list of all the urls on the url to check
+    await getUrlsFromCurrentPage(page,urlToCheck);
+
+    //Fetching the URLs in parallel 
+    for (let i = 0;i<numberOfThreads;i++){
+      console.log("Started Parallel Execution To List Of URLs For Thread : " + i+1);
+      arrayOfPromises[i]=getURLs(page,i+1)
+    }
+
+    //Wait untill fetch url parallel process is finished
+    await processParallel(arrayOfPromises);
+    
+  } else{
+    //Converts the existing list of URLs from CSV to Map
+    urlCheckedMap = await convertCsvToMap(inputFile);      
+  }
   
-  hashMap = await fetchResponse(hashMap);
+  //Fetched the response of the URLs in parallel
+  for (let i = 0;i<numberOfThreads;i++){
+    console.log("Started Parallel Execution To Fetch Response For Thread : " + i+1);
+    arrayOfPromises[i]=fetchResponse(i+1)
+  }
   
-  await createCSVFile(hashMap);
+  //Wait untill url response is fetched for all the threads
+  await processParallel(arrayOfPromises);
+
+  //Generate a CSV file of all the reponses fetched
+  await createCSVFile(responseCheckedMap);
+}
+
+/*
+Description: This function process all the promises passed in "arrayOfPromises" in parallel
+*/
+async function processParallel(arrayOfPromises) {
+  console.time('Processing Parallel')
+  await Promise.all(arrayOfPromises)
+  console.timeEnd('Processing Parallel')
+  console.log('Processing Parallel Complete  \n')
+  return;
 }
 
 /*
 Description : Get the value of an attribute
+@element: Playwright page element
+@attribute: Attribute to be found
+return:
+@textValue: : Text Value of the attribute of the given element
 */
 async function getAttribute(element,attribute) {
   const textValue =  await (await element).getAttribute(attribute).then(async function(value) {
@@ -36,159 +94,208 @@ async function getAttribute(element,attribute) {
 }
 
 /*
-Description : Get the list of URLs
+Description : Get the list of URLs recursively on a given URL
+parameters: 
+@page: Playwright page object
+@thread: Active thread index
+return : 
+@urlCheckedMap : List of allt the URLs recursively found on the base url given in global parameter "urlToCheck"
 */
-async function getURLs(page) {
-  
-  const attribute ="href";
-  console.log("Fetching the URLs and saving in hash map")
+async function getURLs(page,thread) {
 
-  console.log("URL To check : " + urlToCheck);
+  //Limits the execution to the validation of maxURLCheck
+  if (maxURLCheck==""){
+    maxURLCheck == 100000000;
+  }
+  if (urlNotCheckedMap.size>0 && urlCheckedMap.size<maxURLCheck){
+    //Fetches the URL to Check
+    let urlCheck = "";
+    // let urlCheck = urlNotCheckedMap.entries().next().value.toString().split(",")[0];
+    urlNotCheckedMap.forEach (async function(value, key) {
+      urlCheck = key;
+    });
+
+    //Deletes the URL to be checked from not checked map
+    urlNotCheckedMap.delete(urlCheck);
+
+    await getUrlsFromCurrentPage(page,urlCheck);
+  
+    console.log("URL Not Checked Map Size : " + urlNotCheckedMap.size + " for thread : " + thread)
+    await getURLs(page,thread);
+  }else{
+    urlNotCheckedMap.forEach (async function(value, key) {
+      urlCheckedMap.set(key,value);
+    });
+  }
+}
+
+/*
+Description: This function returns all the urls on a given page
+Parameter:
+@page: Playwright page object
+@urlcheck: Page URL from which all the links to be fetched
+@thread: Active thread index
+return : 
+@urlCheckedMap : List of allt the URLs found on the base url given in global parameter "urlToCheck"
+*/
+async function getUrlsFromCurrentPage(page, urlToCheck){
   try{
 
+    // Loads the page and get the page load time
     let pageLoadStartTime = new Date().getTime();
-    await page.goto(urlToCheck);
+    await page.goto(urlToCheck, { timeout: pageLoadTimeout });
     let pageTitle = await page.title();
     let pageLoadEndTime = new Date().getTime();
+    let pageLoadtime = pageLoadEndTime - pageLoadStartTime;    
 
-    let pageLoadtime = pageLoadEndTime - pageLoadStartTime;
-    //Base URL Check
-    let links = await page.$$(getURLCheckXpath());
-    // if (baseUrlCheck == ""){
-    //   links = await page.$$('a[href]');
-    // }else{
-    //   //Links will be filtered if base url is not there
-    //   links = await page.$$("//a[contains(@href,'" + baseUrlCheck + "')]");
-    // }
+    //Fetchs the list of all the URLs on a given page and fetched the link find time
+    let elements = await page.$$(xpathWithBaseUrlCheck);
     let linkFetchEndTime = new Date().getTime();
     let linkFetchTime = linkFetchEndTime - pageLoadEndTime;
-
     urlCheckedMap.set(urlToCheck, pageTitle + "," + urlToCheck + "," + pageLoadtime + "," + linkFetchTime + ",Checked");
-    for (let element of links){
-      let link = await getAttribute(element,attribute);
+    for (let element of elements){
+      let link = await getAttribute(element,urlsAttribute);
       if ((urlCheckedMap.get(link) == undefined)){
-        if (link.split("/").length <= (urlLevelCheck + 2)){
+        //Ignores the URLs based on the level check passed in the global variable "urlLevelCheck"
+        if (link.split("/").length <= (urlLevelCheck + 3)){
           urlNotCheckedMap.set(link,pageTitle + "," + urlToCheck + "," + pageLoadtime + "," + linkFetchTime + ",Not Checked");
-          console.log("URL Check Not Started : " + link);
         }else{
           urlCheckedMap.set(link, pageTitle + "," + urlToCheck + "," + pageLoadtime + "," + linkFetchTime + ",Checked: Based on URL Level Check " + urlLevelCheck);
-          console.log("URL Check Completed : " + link);
         }
       }
       
     }
-
   }catch(err){
-    urlCheckedMap.set(urlToCheck,err.toString());
+    //This condition validates if the URL is already processed by another thread
+    if (!err.toString().includes("Navigation interrupted by another one")){
+      urlCheckedMap.set(urlToCheck,",Error,Error,Error,Error Occurred : " + err.toString().replace(/,/g,"").replace(/(?:\r\n|\r|\n)/g, ""));
+    }
+    
   }
-  
-  console.log("urlNotCheckedMap size : " + urlNotCheckedMap.size)
-  if (urlNotCheckedMap.size>0 && urlNotCheckedMap.size<150){
-    urlToCheck = "";
-    urlNotCheckedMap.forEach (async function(value, key) {
-      urlToCheck = key;
-    });
-    urlNotCheckedMap.delete(urlToCheck);
-    await getURLs(page);
-  }else{
-    urlNotCheckedMap.forEach (async function(value, key) {
-      urlCheckedMap.set(key,value + ",Completed");
-    });
-  }
-  return urlCheckedMap;
 }
 
-
+/*
+Description: Get xpath to fetch the url based on the base on the urls passed in the global parameter "baseUrlCheck"
+return:
+@xpath: xpath of the Urls to be fetched based on multiple base url check
+*/
 function getURLCheckXpath(){
   let xpath = "";
   if (baseUrlCheck == ""){
     xpath = "a[href]";
   }else{
     let  urls = baseUrlCheck.split(",");
-    console.log("URLs : " + urls);
-    console.log("URLs : " + urls.length);
-    let conditions = "contains(@href,'" + urls[0] + "')"; 
+    let xpathConditions = "contains(@href,'" + urls[0] + "')"; 
     for (let i = 1; i < urls.length; i++) {
-      console.log("conditions : " + conditions);
-      conditions = conditions + " or " + "contains(@href,'" + urls[i] + "')"
+      xpathConditions = xpathConditions + " or " + "contains(@href,'" + urls[i] + "')"
     }
-    xpath = "//a[" + conditions + "]";
+    xpath = "//a[" + xpathConditions + "]";
   } 
-  console.log("Xpath: " + xpath);
+  console.log("Xpath of the URLs to be fetched : " + xpath);
   return xpath;
-  // if (baseUrlCheck.split(",").length==0){
-  //   xpath = "a[href]";
-  //   //Links will be filtered if base url is not there
-  //   links = await page.$$("//a[contains(@href,'" + baseUrlCheck + "')]");
-  // }
+}
+
+
+/*
+Description: Fetched the API response/Info/Error of a given URL
+return:
+@responseCheckedMap : Hash Map containing the response/info/error of all the URLs
+*/
+async function fetchResponse(thread){ 
+  if (urlCheckedMap.size > 0){
+    let startTime = new Date().getTime();
+    let urlKey = "";
+    let urlKeyValue = "";
+
+    //Fetched the URL to check response
+    urlCheckedMap.forEach (async function(value, key) {
+      urlKey = key;
+      urlKeyValue = value;
+    });
+     
+    urlCheckedMap.delete(urlKey);
+    
+    //Sets timeout for fetch response
+    setTimeout(() => controller.abort, fetchResponseTimeout)
+
+    //Get the reponse of a fetch request
+    let responseCode = "";
+    await fetch(urlKey,{ signal: controller.signal})
+    .then(response => responseCode =  response.status.toString() + ":" + response.statusText.toString())
+    .then(data => {
+      responseCode= data; // do something with the API response data
+    })
+    .catch(error => {
+      responseCode = error; // handle any errors that occurred during the request
+    });
+
+    let endTime = new Date().getTime();
+    responseCheckedMap.set(urlKey,urlKeyValue + "," + responseCode + "," + (endTime-startTime));
+  
+    //Recursively fetched the response of the URLs untill response is fetched for all the URLs
+    await fetchResponse(thread);
+  }else{
+    console.log("Execution finished for thread " + thread +  " as response Fetched for all the URLs")
+  }
 }
 
 /*
-Description : Filter the hasmap based on URL check
+Description : Create CSV File from HashMap
+return: 
+@outputFile : Creates an output file with the name passed in the parameter "outputFile"
 */
-async function filterUrlLevelCheck(hashmap,urlLevelCheck){ 
-  console.log("URL Count Before URL Level Check Filter : " + hashmap.size)
-  if (urlLevelCheck>0){
-    let map = new Map();
-    hashmap.forEach (function(value, key) {
-      if (key.split("/").length <= (urlLevelCheck + 2)){
-        map.set(key,value);
-      }
-    })
-    console.log("URL Count After URL Level Check Filter : " + map.size)
-    return map;
-    }else{
-      console.log("URL Count After URL Level Check Filter : " + hashmap.size)
-      return hashmap;
-    }
-  }
-
-  /*
-  Description : Fetch URL Response
-  */
-  async function fetchResponse(hashMap){
-    console.log("Starting to fetch response")
-    let map = new Map();
-    let keys= "";
-    hashMap.forEach (async function(value, key) {
-      keys = keys + key + ",";  
-    });
-    let k = keys.split(",");
-    for (let i = 0; i < k.length-1; i++) {
-      let startTime = new Date().getTime();
-      let key = k[i];
-      let responseCode = "";
-      await fetch(key)
-      .then(response => responseCode =  response.status.toString())
-      .then(data => {
-        responseCode= data
-        // do something with the API response data
-      })
-      .catch(error => {
-        responseCode = error
-        // handle any errors that occurred during the request
-      });
-      let endTime = new Date().getTime();
-      map.set(key,hashMap.get(key) + "," + responseCode + "," + (endTime-startTime));
-    }
-    console.log("Response fetched for all the URLs");
-    return map;
-  }
-
-  /*
-  Description : Create CSV File from HashMap
-  */
-  async function createCSVFile(hashMap){
-    let data = "URL,PageTitle,PageURL,PageLoadTime(ms),PageLinkFetchTime(ms),URLValidationStatus,ResponseCode,ResponseTime(ms)\n";
-    hashMap.forEach (function(value, key) {
-        data = data + key + "," + value + "\n";
-    });
-    
-    fs.writeFile('Output.csv', data, (err) => {          
-        // In case of a error throw err.
-        if (err) throw err;
-    })
-    console.log("File Created");
-  }
-
+async function createCSVFile(dataMap){
+  //List of headers to be added in CSV
+  let headers = "URL,PageTitle,PageURL,PageLoadTime(ms),PageLinkFetchTime(ms),URLValidationStatus,ResponseCode,ResponseTime(ms)\n"; 
+  let data = headers;
+  dataMap.forEach (function(value, key) {
+      data = data + key + "," + value + "\n";
+  });
   
+  fs.writeFile(outputFile, data, (err) => {          
+      // In case of a error throw err.
+      if (err) throw err;
+  })
+  console.log("File " + outputFile + " Created");
+}
+
+
+/*
+Description : Reads Data from a file
+*/
+async function readData(fileName){
+  return new Promise(resolve=>{
+    fs.readFile(fileName,'utf8',function(err,data){
+      if(err) throw err;
+      resolve(data);
+    });
+  });
+}
+
+/*
+Description: Converts the data from csv file into a Map
+Parameters: 
+@fileName : Name of the file to be converted to csv
+return:
+@dataMap: Map containing the CSV Data
+*/
+async function convertCsvToMap(fileName){
+  let dataMap = new Map();
+  const csvData = (await readData(fileName)).toString().split("\n");
+  for (let line of csvData){
+    //Filters the blank line and headers from the CSV
+    if (!(line.toString() == "") && !(line.toString().startsWith("URL"))) {
+      let fields = line.split(",");
+      let key = fields[0];
+      let value = "";
+      for (let i=1; i<fields.length-2 ; i++){
+        value = value + fields[i];
+        if (!(i==(fields.length-3))){
+          value = value + ",";
+        }
+      }
+      dataMap.set(key,value);
+    }
+  }
+  return dataMap;
+}
